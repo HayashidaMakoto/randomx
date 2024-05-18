8db296f7df01ac0f192d66c1a9dcfd7a488ee212
Hayashida Makoto <hayashidamakoto@proton.me>
2024-05-18 12:33:24 +0200

    Comment benches

---
+++ benches/lib.rs
@@ -0,0 +1,15 @@
+// #![feature(test)]
+// extern crate test;
+
+// #[cfg(test)]
+// use test::Bencher;
+
+// #[bench]
+// fn bench_aes_generator_1r(_b: &mut Bencher) {
+//     // FIXME
+// }
+
+// #[bench]
+// fn bench_aes_generator_4r(_b: &mut Bencher) {
+//     // FIXME
+// }
