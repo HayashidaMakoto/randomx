99b0b921f7430b63708e6ffcbddf4954d6c186f0
Hayashida Makoto <hayashidamakoto@proton.me>
2024-05-18 12:00:06 +0200

    Make constants public and implement aes_generator_1r

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,79 @@
-// pub mod parameters;
+use aes::cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit};
+use aes::Aes128;
+
+pub mod parameters;
 // pub mod registers;
+
+/// Implement [AesGenerator1R](https://github.com/tevador/RandomX/blob/master/doc/specs.md#32-aesgenerator1r).
+///
+/// Specification goes like this:
+///
+/// ### 3.2 AesGenerator1R
+/// AesGenerator1R produces a sequence of pseudo-random bytes.
+///
+/// The internal state of the generator consists of 64 bytes arranged into four
+/// columns of 16 bytes each. During each output iteration, every column is
+/// decrypted (columns 0, 2) or encrypted (columns 1, 3) with one AES round
+/// using the following round keys (one key per column):
+///
+/// ```text
+/// key0 = 53 a5 ac 6d 09 66 71 62 2b 55 b5 db 17 49 f4 b4
+/// key1 = 07 af 7c 6d 0d 71 6a 84 78 d3 25 17 4e dc a1 0d
+/// key2 = f1 62 12 3f c6 7e 94 9f 4f 79 c0 f4 45 e3 20 3e
+/// key3 = 35 81 ef 6a 7c 31 ba b1 88 4c 31 16 54 91 16 49
+/// ```
+/// These keys were generated as:
+/// ```text
+/// key0, key1, key2, key3 = Hash512("RandomX AesGenerator1R keys")
+/// ```
+///
+/// Single iteration produces 64 bytes of output which also become the new generator state.
+/// ```text
+/// state0 (16 B)    state1 (16 B)    state2 (16 B)    state3 (16 B)
+///      |                |                |                |
+///  AES decrypt      AES encrypt      AES decrypt      AES encrypt
+///    (key0)           (key1)           (key2)           (key3)
+///      |                |                |                |
+///      v                v                v                v
+///   state0'          state1'          state2'          state3'
+/// ```
+pub fn aes_generator_1r(input: [u8; 64]) -> [u8; 64] {
+    // key0 - decrypt
+    let key0 = GenericArray::from(parameters::AES_GENERATOR_1R_K0);
+    let cipher = Aes128::new(&key0);
+    let state0: [u8; 16] = input[0..16].try_into().unwrap();
+    let mut state0 = GenericArray::from(state0);
+    cipher.decrypt_block(&mut state0);
+
+    // key1 - encrypt
+    let key1 = GenericArray::from(parameters::AES_GENERATOR_1R_K1);
+    let cipher = Aes128::new(&key1);
+    let state1: [u8; 16] = input[16..32].try_into().unwrap();
+    let mut state1 = GenericArray::from(state1);
+    cipher.encrypt_block(&mut state1);
+
+    // key2 - decrypt
+    let key2 = GenericArray::from(parameters::AES_GENERATOR_1R_K2);
+    let cipher = Aes128::new(&key2);
+    let state2: [u8; 16] = input[32..48].try_into().unwrap();
+    let mut state2 = GenericArray::from(state2);
+    cipher.decrypt_block(&mut state2);
+
+    // key3 - encrypt
+    let key3 = GenericArray::from(parameters::AES_GENERATOR_1R_K3);
+    let cipher = Aes128::new(&key3);
+    let state3: [u8; 16] = input[48..64].try_into().unwrap();
+    let mut state3 = GenericArray::from(state3);
+    cipher.encrypt_block(&mut state3);
+
+    let mut output: [u8; 64] = [0; 64];
+    output[0..16].copy_from_slice(state0.as_slice());
+    output[16..32].copy_from_slice(state1.as_slice());
+    output[32..48].copy_from_slice(state2.as_slice());
+    output[48..64].copy_from_slice(state3.as_slice());
+
+    output
+}
 
 #[cfg(test)]
 pub mod test;
--- src/parameters.rs
+++ src/parameters.rs
@@ -2,49 +2,66 @@
 //! They are listed on [RandomX
 //! specs](https://github.com/tevador/RandomX/blob/master/doc/specs.md)
 /// The number of 1 KiB Argon2 blocks in the Cache
-const RANDOMX_ARGON_MEMORY: usize = 262144;
+pub const RANDOMX_ARGON_MEMORY: usize = 262144;
 
 /// The number of Argon2d iterations for Cache initialization
-const RANDOMX_ARGON_ITERATIONS: usize = 3;
+pub const RANDOMX_ARGON_ITERATIONS: usize = 3;
 
 /// The number of parallel lanes for Cache initialization
-const RANDOMX_ARGON_LANES: usize = 1;
+pub const RANDOMX_ARGON_LANES: usize = 1;
 
 /// Argon2 salt
-const RANDOMX_ARGON_SALT: [u8; 8] = [b'R', b'a', b'n', b'd', b'o', b'm', b'X', b'\x03'];
+pub const RANDOMX_ARGON_SALT: [u8; 8] = [b'R', b'a', b'n', b'd', b'o', b'm', b'X', b'\x03'];
 
 /// The number of random Cache accesses per Dataset item
-const RANDOMX_CACHE_ACCESSES: usize = 8;
+pub const RANDOMX_CACHE_ACCESSES: usize = 8;
 
 /// Target latency for SuperscalarHash (in cycles of the reference CPU)
-const RANDOMX_SUPERSCALAR_LATENCY: usize = 170;
+pub const RANDOMX_SUPERSCALAR_LATENCY: usize = 170;
 
 /// Dataset base size in bytes
-const RANDOMX_DATASET_BASE_SIZE: usize = 2147483648;
+pub const RANDOMX_DATASET_BASE_SIZE: usize = 2147483648;
 
 /// Dataset extra size in bytes
-const RANDOMX_DATASET_EXTRA_SIZE: usize = 33554368;
+pub const RANDOMX_DATASET_EXTRA_SIZE: usize = 33554368;
 
 /// The number of instructions in a RandomX program
-const RANDOMX_PROGRAM_SIZE: usize = 256;
+pub const RANDOMX_PROGRAM_SIZE: usize = 256;
 
 /// The number of iterations per program
-const RANDOMX_PROGRAM_ITERATIONS: usize = 2048;
+pub const RANDOMX_PROGRAM_ITERATIONS: usize = 2048;
 
 /// The number of programs per hash
-const RANDOMX_PROGRAM_COUNT: usize = 8;
+pub const RANDOMX_PROGRAM_COUNT: usize = 8;
 
 /// Jump condition mask size in bits
-const RANDOMX_JUMP_BITS: usize = 8;
+pub const RANDOMX_JUMP_BITS: usize = 8;
 
 /// Jump condition mask offset in bits
-const RANDOMX_JUMP_OFFSET: usize = 8;
+pub const RANDOMX_JUMP_OFFSET: usize = 8;
 
 /// Scratchpad L3 size in bytes
-const RANDOMX_SCRATCHPAD_L3: usize = 2097152;
+pub const RANDOMX_SCRATCHPAD_L3: usize = 2097152;
 
 /// Scratchpad L2 size in bytes
-const RANDOMX_SCRATCHPAD_L2: usize = 262144;
+pub const RANDOMX_SCRATCHPAD_L2: usize = 262144;
 
 /// Scratchpad L1 size in bytes
-const RANDOMX_SCRATCHPAD_L1: usize = 16384;
+pub const RANDOMX_SCRATCHPAD_L1: usize = 16384;
+
+/// Keys used for AesGenerator1R
+pub const AES_GENERATOR_1R_K0: [u8; 16] = [
+    0x53, 0xa5, 0xac, 0x6d, 0x09, 0x66, 0x71, 0x62, 0x2b, 0x55, 0xb5, 0xdb, 0x17, 0x49, 0xf4, 0xb4,
+];
+
+pub const AES_GENERATOR_1R_K1: [u8; 16] = [
+    0x07, 0xaf, 0x7c, 0x6d, 0x0d, 0x71, 0x6a, 0x84, 0x78, 0xd3, 0x25, 0x17, 0x4e, 0xdc, 0xa1, 0x0d,
+];
+
+pub const AES_GENERATOR_1R_K2: [u8; 16] = [
+    0xf1, 0x62, 0x12, 0x3f, 0xc6, 0x7e, 0x94, 0x9f, 0x4f, 0x79, 0xc0, 0xf4, 0x45, 0xe3, 0x20, 0x3e,
+];
+
+pub const AES_GENERATOR_1R_K3: [u8; 16] = [
+    0x35, 0x81, 0xef, 0x6a, 0x7c, 0x31, 0xba, 0xb1, 0x88, 0x4c, 0x31, 0x16, 0x54, 0x91, 0x16, 0x49,
+];
