b577d6fa5dc9a38a0d2d934f7674527459e1ce0d
Hayashida Makoto <hayashidamakoto@proton.me>
2024-05-22 22:46:23 +0200

    Helpers: update documentation

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,14 @@
 use crate::parameters::{FLOAT_EXPONENT_MASK, FLOAT_MANTISSA_MASK};
 
+/// The fraction has full 52 bits of precision and the exponent value ranges
+/// from 0 to 31. These values are obtained from the initialization quadword (in
+/// little endian format) according to Table below:
+/// -----------------------
+/// | bits 	| description |
+/// | 0-51 	| mantissa    |
+/// | 52-58 | (reserved)  |
+/// | 59-63 | exponent    |
+/// -----------------------
 pub fn f64_from_u64(v: u64) -> f64 {
     // We must shift the 4 bits of the exponent on the right of the fraction
     // get bits 59-63
